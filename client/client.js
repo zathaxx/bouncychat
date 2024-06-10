7d1793c2aac7aa03f832cfe1ec4eaaf041905570
Nova <me@josias.dev>
2024-06-10 10:08:17 -0700

    Add notifications

--- client/client.js
+++ client/client.js
@@ -1,3 +1,10 @@
+const NOTIFICATION_TIMEOUT = 4000
+
+function sendNotification(title, body) {
+    var notification = new Notification(title, { body: body, icon: 'logo.png ' })
+    setTimeout(notification.close.bind(notification), NOTIFICATION_TIMEOUT);
+}
+
 let socket;
 const connect = function() {
     return new Promise((resolve, reject) => {
@@ -25,6 +32,11 @@ const connect = function() {
 		parent.appendChild(mEl)
                 document.getElementById('chatbox').appendChild(parent)
 		scrollBottom()
+
+		const name = localStorage.getItem('displayname')
+		if (parsedData.name !== name) {
+		    sendNotification(parsedData.name, parsedData.message)
+		}
             }
         }
 
@@ -66,6 +78,14 @@ document.getElementById('displayname').addEventListener('change', function (e) {
 window.onload = () => {
     const name = localStorage.getItem('displayname')
     document.getElementById('displayname').value = name
+
+    if (Notification.permission !== 'granted') {
+        Notification.requestPermission().then(function (result) {
+            if (Notification.permission === 'granted') {
+                sendNotification('Bouncy Chat', 'Notifications are now enabled. Prepare for updates!')
+            }
+        })
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
