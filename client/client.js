b3ef2f4abbd414aae3713bff322a8fff46a15620
Nova <me@josias.dev>
2024-06-09 20:41:43 -0700

    Add new styling; basic display name support

--- client/client.js
+++ client/client.js
@@ -8,7 +8,7 @@ const connect = function() {
         socket = new WebSocket(socketUrl);
 
         socket.onopen = (e) => {
-            socket.send(JSON.stringify({ "loaded" : true }));
+            //socket.send(JSON.stringify({ "loaded" : true }));
             resolve();
         }
 
@@ -16,9 +16,16 @@ const connect = function() {
             console.log(data);
             let parsedData = JSON.parse(data.data);
             if (parsedData.append === true) {
-                const newEl = document.createElement('p');
-                newEl.textContent = parsedData.returnText;
-                document.getElementById('websocket-response').appendChild(newEl);
+		const parent = document.createElement('div')
+		parent.tabIndex = "0"
+                const mEl = document.createElement('p');
+                mEl.textContent = parsedData.returnText;
+		const uEl = document.createElement('span')
+		uEl.textContent = parsedData.name
+		parent.appendChild(uEl)
+		parent.appendChild(mEl)
+                document.getElementById('chatbox').appendChild(parent);
+		scrollBottom()
             }
         }
 
@@ -34,15 +41,32 @@ const isOpen = function(ws) {
     return ws.readyState === ws.OPEN 
 }
 
+function scrollBottom() {
+    let box = document.getElementById('chatbox')
+    box.scrollTop = box.scrollHeight
+}
+
 function sendMessage() {
     let message_el = document.getElementById('message-text')
+    let name = document.getElementById('displayname').value
     const message = message_el.value
     message_el.value = ''
     if(isOpen(socket)) {
         socket.send(JSON.stringify({
             "message": message,
+	    "name": name
         }))
     }
+}
+
+document.getElementById('displayname').addEventListener('change', function (e) {
+    const name = document.getElementById('displayname').value
+    localStorage.setItem('displayname', name)
+})
+
+window.onload = () => {
+    const name = localStorage.getItem('displayname')
+    document.getElementById('displayname').value = name
 }
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -55,4 +79,5 @@ document.addEventListener('DOMContentLoaded', function () {
 	    sendMessage()
 	}
     })
+    scrollBottom()
 });
--- index.js
+++ index.js
@@ -1,23 +1,29 @@
+const path = require('path');
 const express = require('express')
+
+const db = require('./db')
+
 const app = express()
 const port = 3000
 
 const expressWS = require('express-ws')(app);
 
-const db = require('./db')
-
 let wss = expressWS.getWss()
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'client'));
 
 app.ws('/ws', async function(ws, req) {
     let client = new db.Client;
     await client.open()
     ws.on('message', async function(msg) {
         await client.query()
-	const message = JSON.parse(msg).message
+	const data = JSON.parse(msg)
 	wss.clients.forEach(function (sock) {
 	    sock.send(JSON.stringify({
 		"append": true,
-		"returnText": message
+		"returnText": data.message,
+		"name": data.name
 	    }))
 	});
     });
@@ -25,6 +31,14 @@ app.ws('/ws', async function(ws, req) {
       await client.close()
     });
 });
+
+app.get('/', (req, res) => {
+    //res.sendFile('index.html', {root: path.join(__dirname, 'client')})
+    res.render('index', {
+	chat_log: [
+	    { name: 'admin', message: 'Hello! Welcome to Bouncy Chat, a communications platform powered by WebSocket. Be nice and enjoy the conversation!'}
+	]})
+})
 
 app.use(express.static('client'))
 
