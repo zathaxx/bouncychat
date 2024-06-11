35d0237bf7d77c854d0804fcf67dca357aeafd18
Nova <me@josias.dev>
2024-06-11 12:29:10 -0700

    Load page on enter key

--- static/main.js
+++ static/main.js
@@ -1,4 +1,14 @@
-document.getElementById('loadroom').addEventListener('click', (e) => {
+function loadroom() {
     const roomid = document.getElementById('roomid').value
-    window.location.href += roomid
+    window.location = roomid
+}
+
+document.getElementById('loadroom').addEventListener('click', (e) => {
+    loadroom()
+})
+
+document.getElementById('roomid').addEventListener('keydown', (e) => {
+    if (e.keyCode === 13) {
+	loadroom()
+    }
 })
